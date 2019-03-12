80198d3fcfcf136e50264a42ff356f4fa2f6bc41
João Monteiro <joaoagrom@gmail.com>
2019-03-12 12:07:02 -0300

    added AudioManager Handling audioFocusChange

--- app/src/main/java/com/example/android/miwok/FamilyActivity.java
+++ app/src/main/java/com/example/android/miwok/FamilyActivity.java
@@ -15,6 +15,8 @@
  */
 package com.example.android.miwok;
 
+import android.content.Context;
+import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -26,7 +28,35 @@ import java.util.ArrayList;
 
 public class FamilyActivity extends AppCompatActivity {
 
-    MediaPlayer mediaPlayer;
+    MediaPlayer mMediaPlayer;
+    AudioManager mAudioManager;
+
+    MediaPlayer.OnCompletionListener mOnCompletionListener = new MediaPlayer.OnCompletionListener() {
+        @Override
+        public void onCompletion(MediaPlayer mediaPlayer) {
+            releaseMediaPlayer();
+        }
+    };
+
+    AudioManager.OnAudioFocusChangeListener mOnAudioFocusChangeListener = new AudioManager.OnAudioFocusChangeListener() {
+        @Override
+        public void onAudioFocusChange(int focusChange) {
+            switch (focusChange) {
+                case AudioManager.AUDIOFOCUS_GAIN:
+                    mMediaPlayer.start();
+                    break;
+                case AudioManager.AUDIOFOCUS_LOSS:
+                    releaseMediaPlayer();
+                    break;
+                case AudioManager.AUDIOFOCUS_LOSS_TRANSIENT:
+                case AudioManager.AUDIOFOCUS_LOSS_TRANSIENT_CAN_DUCK:
+                    mMediaPlayer.pause();
+                    mMediaPlayer.seekTo(0);
+                    break;
+                default:
+            }
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,26 +65,28 @@ public class FamilyActivity extends AppCompatActivity {
 
         final ArrayList<Word> mFamily = new ArrayList<>();
 
-        mFamily.add(new Word("әpә",
-                "father", R.drawable.family_father, R.raw.family_father));
-        mFamily.add(new Word("әṭa",
-                "mother", R.drawable.family_mother, R.raw.family_mother));
-        mFamily.add(new Word("angsi",
-                "son", R.drawable.family_son, R.raw.family_son));
-        mFamily.add(new Word("tune",
-                "daughter", R.drawable.family_daughter, R.raw.family_daughter));
-        mFamily.add(new Word("taachi",
-                "older brother", R.drawable.family_older_brother, R.raw.family_older_brother));
-        mFamily.add(new Word("chalitti",
-                "younger brother", R.drawable.family_younger_brother, R.raw.family_younger_brother));
-        mFamily.add(new Word("teṭe",
-                "older sister", R.drawable.family_older_sister, R.raw.family_older_sister));
-        mFamily.add(new Word("kolliti",
-                "younger sister", R.drawable.family_younger_sister, R.raw.family_younger_sister));
-        mFamily.add(new Word("ama",
-                "grandmother", R.drawable.family_grandmother, R.raw.family_grandmother));
-        mFamily.add(new Word("paapa",
-                "grandfather", R.drawable.family_grandfather, R.raw.family_grandfather));
+        mAudioManager = (AudioManager) getApplication().getSystemService(Context.AUDIO_SERVICE);
+
+        mFamily.add(new Word("әpә", "father",
+                R.drawable.family_father, R.raw.family_father));
+        mFamily.add(new Word("әṭa", "mother",
+                R.drawable.family_mother, R.raw.family_mother));
+        mFamily.add(new Word("angsi", "son",
+                R.drawable.family_son, R.raw.family_son));
+        mFamily.add(new Word("tune", "daughter",
+                R.drawable.family_daughter, R.raw.family_daughter));
+        mFamily.add(new Word("taachi", "older brother",
+                R.drawable.family_older_brother, R.raw.family_older_brother));
+        mFamily.add(new Word("chalitti", "younger brother",
+                R.drawable.family_younger_brother, R.raw.family_younger_brother));
+        mFamily.add(new Word("teṭe", "older sister",
+                R.drawable.family_older_sister, R.raw.family_older_sister));
+        mFamily.add(new Word("kolliti", "younger sister",
+                R.drawable.family_younger_sister, R.raw.family_younger_sister));
+        mFamily.add(new Word("ama", "grandmother",
+                R.drawable.family_grandmother, R.raw.family_grandmother));
+        mFamily.add(new Word("paapa", "grandfather",
+                R.drawable.family_grandfather, R.raw.family_grandfather));
 
         ListView listView = findViewById(R.id.list);
 
@@ -64,9 +96,33 @@ public class FamilyActivity extends AppCompatActivity {
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                mediaPlayer = MediaPlayer.create(getApplication(), mFamily.get(i).getAudioResourceId());
-                mediaPlayer.start();
+                releaseMediaPlayer();
+
+                int result = mAudioManager.requestAudioFocus(mOnAudioFocusChangeListener,
+                        AudioManager.AUDIOFOCUS_GAIN_TRANSIENT, AudioManager.STREAM_MUSIC);
+
+                if (result == AudioManager.AUDIOFOCUS_REQUEST_GRANTED) {
+                    mMediaPlayer = MediaPlayer.create(getApplication(), mFamily.get(i).getAudioResourceId());
+                    mMediaPlayer.start();
+
+                    mMediaPlayer.setOnCompletionListener(mOnCompletionListener);
+                }
             }
         });
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        releaseMediaPlayer();
+    }
+
+    private void releaseMediaPlayer() {
+        if (mMediaPlayer != null) {
+            mMediaPlayer.release();
+
+            mMediaPlayer = null;
+            mAudioManager.abandonAudioFocus(mOnAudioFocusChangeListener);
+        }
     }
 }
--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -15,6 +15,8 @@
  */
 package com.example.android.miwok;
 
+import android.content.Context;
+import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -26,7 +28,35 @@ import java.util.ArrayList;
 
 public class NumbersActivity extends AppCompatActivity {
 
-    MediaPlayer mediaPlayer;
+    MediaPlayer mMediaPlayer;
+    AudioManager mAudioManager;
+
+    MediaPlayer.OnCompletionListener mOnCompletionListener = new MediaPlayer.OnCompletionListener() {
+        @Override
+        public void onCompletion(MediaPlayer mediaPlayer) {
+            releaseMediaPlayer();
+        }
+    };
+
+    AudioManager.OnAudioFocusChangeListener mOnAudioFocusChangeListener = new AudioManager.OnAudioFocusChangeListener() {
+        @Override
+        public void onAudioFocusChange(int focusChange) {
+            switch (focusChange) {
+                case AudioManager.AUDIOFOCUS_GAIN:
+                    mMediaPlayer.start();
+                    break;
+                case AudioManager.AUDIOFOCUS_LOSS:
+                    releaseMediaPlayer();
+                    break;
+                case AudioManager.AUDIOFOCUS_LOSS_TRANSIENT:
+                case AudioManager.AUDIOFOCUS_LOSS_TRANSIENT_CAN_DUCK:
+                    mMediaPlayer.pause();
+                    mMediaPlayer.seekTo(0);
+                    break;
+                default:
+            }
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,28 +65,64 @@ public class NumbersActivity extends AppCompatActivity {
 
         final ArrayList<Word> mNumbers = new ArrayList<>();
 
-        mNumbers.add(new Word("lutti", "one", R.drawable.number_one, R.raw.number_one));
-        mNumbers.add(new Word("otiiko", "two", R.drawable.number_two, R.raw.number_two));
-        mNumbers.add(new Word("tolooskosu", "three", R.drawable.number_three, R.raw.number_three));
-        mNumbers.add(new Word("oyyisa", "four", R.drawable.number_four, R.raw.number_four));
-        mNumbers.add(new Word("massokka", "five", R.drawable.number_five, R.raw.number_five));
-        mNumbers.add(new Word("temmokka", "six", R.drawable.number_six, R.raw.number_six));
-        mNumbers.add(new Word("kenekaku", "seven", R.drawable.number_seven, R.raw.number_seven));
-        mNumbers.add(new Word("kawinta", "eight", R.drawable.number_eight, R.raw.number_eight));
-        mNumbers.add(new Word("wo´e", "nine", R.drawable.number_nine, R.raw.number_nine));
-        mNumbers.add(new Word("na´áacha", "ten", R.drawable.number_ten, R.raw.number_ten));
+        mAudioManager = (AudioManager) getApplication().getSystemService(Context.AUDIO_SERVICE);
+
+        mNumbers.add(new Word("lutti", "one",
+                R.drawable.number_one, R.raw.number_one));
+        mNumbers.add(new Word("otiiko", "two",
+                R.drawable.number_two, R.raw.number_two));
+        mNumbers.add(new Word("tolooskosu", "three",
+                R.drawable.number_three, R.raw.number_three));
+        mNumbers.add(new Word("oyyisa", "four",
+                R.drawable.number_four, R.raw.number_four));
+        mNumbers.add(new Word("massokka", "five",
+                R.drawable.number_five, R.raw.number_five));
+        mNumbers.add(new Word("temmokka", "six",
+                R.drawable.number_six, R.raw.number_six));
+        mNumbers.add(new Word("kenekaku", "seven",
+                R.drawable.number_seven, R.raw.number_seven));
+        mNumbers.add(new Word("kawinta", "eight",
+                R.drawable.number_eight, R.raw.number_eight));
+        mNumbers.add(new Word("wo´e", "nine",
+                R.drawable.number_nine, R.raw.number_nine));
+        mNumbers.add(new Word("na´áacha", "ten",
+                R.drawable.number_ten, R.raw.number_ten));
 
         ListView listView = findViewById(R.id.list);
 
         WordAdapter adapter = new WordAdapter(this, mNumbers, R.color.category_numbers);
-        listView.setAdapter(adapter);
 
+        listView.setAdapter(adapter);
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                mediaPlayer = MediaPlayer.create(getApplication(), mNumbers.get(i).getAudioResourceId());
-                mediaPlayer.start();
+                releaseMediaPlayer();
+
+                int result = mAudioManager.requestAudioFocus(mOnAudioFocusChangeListener,
+                        AudioManager.AUDIOFOCUS_GAIN_TRANSIENT, AudioManager.STREAM_MUSIC);
+
+                if (result == AudioManager.AUDIOFOCUS_REQUEST_GRANTED) {
+                    mMediaPlayer = MediaPlayer.create(getApplication(), mNumbers.get(i).getAudioResourceId());
+                    mMediaPlayer.start();
+                    mMediaPlayer.setOnCompletionListener(mOnCompletionListener);
+                }
             }
         });
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        releaseMediaPlayer();
+    }
+
+    private void releaseMediaPlayer() {
+        if (mMediaPlayer != null) {
+            mMediaPlayer.release();
+            mMediaPlayer = null;
+
+            mAudioManager.abandonAudioFocus(mOnAudioFocusChangeListener);
+        }
     }
 }
